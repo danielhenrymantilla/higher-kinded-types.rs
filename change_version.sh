018788097d13e2837ff40bf651c179396b2c55a8
Daniel Henry-Mantilla <daniel.henry.mantilla@gmail.com>
2023-07-03 14:20:59 +0200

    rc4

--- change_version.sh
+++ change_version.sh
@@ -2,13 +2,21 @@
 
 set -euxo pipefail
 
+version="${1:?Usage: \`\"$0\" <version>\`}"
+
 find . \
     -type f \
     -name 'Cargo.toml' \
     -print \
     -a \
     -exec \
-        sed -i -E "s/\"(=)?.*?\"(  # Keep in sync)/\"\\1$1\"\\2/g" '{}' \
-    \;
+        sed -i -E 's/"(=)?.*"(  # Keep in sync)/"\1'"${version}"'"\2/g' '{}' \
+    \; \
+;
+
+sed -i -E \
+    's@higher-kinded-types/[^/]*/@higher-kinded-types/'"${version}"'/@' \
+    README.md \
+;
 
 cargo +stable update -v -w
